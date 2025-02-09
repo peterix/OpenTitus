ad34191c351a8217d21cf8b81ef5751f2fa25831
Petr Mrázek <peterix@gmail.com>
2025-02-09 14:05:09 +0100

    player.c - it is zig now

--- src/player.zig
+++ src/player.zig
@@ -20,7 +20,7 @@
 // MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
 // General Public License for more details.
 
-// player.c
+// player.zig
 // Handles player movement and keyboard handling
 
 const std = @import("std");
